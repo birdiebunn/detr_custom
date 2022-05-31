2eee3f8af6f7b8b021d86198ddd71ccbea6f0763
birdiebunn <106571832+birdiebunn@users.noreply.github.com>
2022-05-31 12:26:31 +0700

    Update train.py

--- train.py
+++ train.py
@@ -177,6 +177,8 @@ Run on multiple machines:
     # Backbone
     parser.add_argument('--backbone', default='resnet50', type=str,
                         help="Name of the convolutional backbone to use")
+    parser.add_argument('--dilation', action='store_true',
+                        help="If true, we replace stride with dilation in the last convolutional block (DC5)")
     parser.add_argument('--position_embedding', default='sine', type=str, choices=('sine', 'learned'),
                         help="Type of positional embedding to use on top of the image features")
     
