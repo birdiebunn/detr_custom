f656f097bbfd2bfd642af1f61e6886fe10154e12
birdiebunn <106571832+birdiebunn@users.noreply.github.com>
2022-05-31 12:33:14 +0700

    Update train.py

--- train.py
+++ train.py
@@ -183,12 +183,21 @@ Run on multiple machines:
                         help="Type of positional embedding to use on top of the image features")
     
     # Transformer
+    parser.add_argument('--enc_layers', default=6, type=int,
+                        help="Number of encoding layers in the transformer")
+    parser.add_argument('--dec_layers', default=6, type=int,
+                        help="Number of decoding layers in the transformer")
+    parser.add_argument('--dim_feedforward', default=2048, type=int,
+                        help="Intermediate size of the feedforward layers in the transformer blocks")
     parser.add_argument('--hidden_dim', default=256, type=int,
                         help="Size of the embeddings (dimension of the transformer)")
     parser.add_argument('--dropout', default=0.1, type=float,
                         help="Dropout applied in the transformer")
     parser.add_argument('--num_head', default=8, type=int,
                         help="Number of attention heads inside the transformer's attentions")
+    parser.add_argument('--num_queries', default=100, type=int,
+                        help="Number of query slots")
+    parser.add_argument('--pre_norm', action='store_true')
     
     # Loss coefficients
     parser.add_argument('--mask_loss_coef', default=1, type=float)
